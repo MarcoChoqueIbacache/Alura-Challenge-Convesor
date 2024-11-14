14d1c9d1ee4b654dce83c602a61027c3b54216cc
MarcoChoqueIbacache <choqueibacache.marco@gmail.com>
2024-11-14 20:38:41 -0300

    Subiendo Proyecto

---
+++ src/com/marco/app/IniciarApp.java
@@ -0,0 +1,11 @@
+package com.marco.app;
+
+
+
+public class IniciarApp {
+    public static void main(String[] args) {
+        // Crear una instancia de MenuPrincipal y llamar al método iniciar
+        MenuPrincipal inicio = new MenuPrincipal();
+        inicio.iniciar();
+    }
+}
